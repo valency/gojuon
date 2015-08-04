5ebbec293f024d6188eb04eaf8bfa2ab7fec6688
Ye Ding <guiewy@gmail.com>
2015-08-04 20:34:16 +0800

    v0.9.13

---
+++ Gojuon/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gojuon")]
+[assembly: AssemblyDescription("An app for remembering Gojuon.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Valency")]
+[assembly: AssemblyProduct("Gojuon")]
+[assembly: AssemblyCopyright("(C) 2012 Valency, All Right Reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8d05dffb-d588-41e9-aaff-3d91c6efd977")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.9.13")]
+[assembly: AssemblyFileVersion("1.2.9.13")]
